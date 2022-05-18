935026799140ccc9546b61f718c492508429bf57
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-18 15:06:21 +0200

    Revert "3040-extract-pr-code"

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+const core = require('@actions/core');
+const wait = require('./wait');
+
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const merge_commit_number = github.context.payload.pull_request.merge_commit_sha
-    core.info(`merge_commit_number ${merge_commit_number}`)
+    const ms = core.getInput('milliseconds');
+    core.info(`Waiting ${ms} milliseconds ...`);
+
+    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    await wait(parseInt(ms));
+    core.info((new Date()).toTimeString());
+
+    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
