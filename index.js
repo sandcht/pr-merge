cbb6d286e4d2b41c837f55ecb1927fed526238dd
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-20 11:19:55 +0200

    correction du test

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ async function cherry_pick_branch (ReleaseConfiguration,MileStone){
   core.info(`index_Sprint_Actif   = ${index_Sprint_Actif  }`)
   CHERRY_PICK_BRANCH.push(ReleaseConfiguration.slice(index_milestone,index_Sprint_Actif))
   core.info(`CHERRY_PICK_BRANCH = ${CHERRY_PICK_BRANCH}`)
-  core.info(`CHERRY_PICK_BRANCH = ${CHERRY_PICK_BRANCH.[0]}`)
-  core.info(`CHERRY_PICK_BRANCH = ${CHERRY_PICK_BRANCH.[1]}`)
+  core.info(`CHERRY_PICK_BRANCH = ${CHERRY_PICK_BRANCH[0]}`)
+  core.info(`CHERRY_PICK_BRANCH = ${CHERRY_PICK_BRANCH[1]}`)
 
 }
 run();
