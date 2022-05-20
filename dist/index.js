3c1ef05af4e972bd5ae8d446090c0075e6d3a711
sandcht <99471869+sandcht@users.noreply.github.com>
2022-05-20 13:25:23 +0200

    get branch

--- dist/index.js
+++ dist/index.js
@@ -9766,7 +9766,7 @@ async function cherry_pick_base_branch (ReleaseConfiguration,MileStone){
   const index_Sprint_Actif = ReleaseConfiguration.findIndex((config)=> config.isActive==true)
   _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`index_Sprint_Actif= ${index_Sprint_Actif  }`)
   const SubReleaseConfigurationTab = ReleaseConfiguration.slice(index_milestone,index_Sprint_Actif)
-  CHERRY_PICK_BRANCH.push(SubReleaseConfigurationTab.filter((SubReleaseConfiguration)=>SubReleaseConfiguration.branch))
+  CHERRY_PICK_BRANCH.push(SubReleaseConfigurationTab.map((SubReleaseConfiguration,index)=>SubReleaseConfiguration[index].branch))
   _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(JSON.stringify(CHERRY_PICK_BRANCH))
   _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`taille du tableau  == ${CHERRY_PICK_BRANCH.length}`)
   _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(JSON.stringify(ReleaseConfiguration.slice(index_milestone,index_Sprint_Actif)))
